c9a9ad4ac707e788ac8d6916402f41a00ad9ccba
Tom Budon <tom.budon@ens.uvsq.fr>
2022-11-08 15:19:57 +0100

    Ajout des versions d'unroll 4, 8 et 16 pour le dotprod + ajout de Run_benchmark.sh et de plot.gp
    Le Run_benchmark.sh et le plot.gp sont sur le même principe que pour la dgemm.

--- TD2/dotprod/kernels.c
+++ TD2/dotprod/kernels.c
@@ -19,3 +19,88 @@ f64 dotprod_base(f64 *restrict a, f64 *restrict b, u64 n)
   return d;
 }
 
+f64 dotprod_unroll4(f64 *restrict a, f64 *restrict b, u64 n)
+{
+
+  #define UNROLL4 4
+
+  double d = 0.0;
+
+  u64 mod = n % UNROLL4;
+  
+  for (u64 i = 0; i < n-mod; i += UNROLL4) {
+    d += a[i    ] * b[i    ];
+    d += a[i + 1] * b[i + 1];
+    d += a[i + 2] * b[i + 2];
+    d += a[i + 3] * b[i + 3];
+  }
+
+  for (u64 i = n-mod; i < n; i++) {
+    d += a[i] * b[i];
+  }
+
+  return d;
+}
+
+f64 dotprod_unroll8(f64 *restrict a, f64 *restrict b, u64 n)
+{
+
+  #define UNROLL8 8
+
+  double d = 0.0;
+
+  u64 mod = n % UNROLL8;
+  
+  for (u64 i = 0; i < n-mod; i += UNROLL8) {
+    d += a[i    ] * b[i    ];
+    d += a[i + 1] * b[i + 1];
+    d += a[i + 2] * b[i + 2];
+    d += a[i + 3] * b[i + 3];
+    d += a[i + 4] * b[i + 4];
+    d += a[i + 5] * b[i + 5];
+    d += a[i + 6] * b[i + 6];
+    d += a[i + 7] * b[i + 7];
+  }
+
+  for (u64 i = n-mod; i < n; i++) {
+    d += a[i] * b[i];
+  }
+
+  return d;
+}
+
+f64 dotprod_unroll16(f64 *restrict a, f64 *restrict b, u64 n)
+{
+
+  #define UNROLL16 16
+
+  double d = 0.0;
+
+  u64 mod = n % UNROLL16;
+  
+  for (u64 i = 0; i < n-mod; i += UNROLL16) {
+    d += a[i    ] * b[i    ];
+    d += a[i + 1] * b[i + 1];
+    d += a[i + 2] * b[i + 2];
+    d += a[i + 3] * b[i + 3];
+    d += a[i + 4] * b[i + 4];
+    d += a[i + 5] * b[i + 5];
+    d += a[i + 6] * b[i + 6];
+    d += a[i + 7] * b[i + 7];
+    d += a[i + 8] * b[i + 8];
+    d += a[i + 9] * b[i + 9];
+    d += a[i + 10] * b[i + 10];
+    d += a[i + 11] * b[i + 11];
+    d += a[i + 12] * b[i + 12];
+    d += a[i + 13] * b[i + 13];
+    d += a[i + 14] * b[i + 14];
+    d += a[i + 15] * b[i + 15];
+  }
+
+  for (u64 i = n-mod; i < n; i++) {
+    d += a[i] * b[i];
+  }
+
+  return d;
+}
+
---
+++ TD2/dotprod/plot.gp
@@ -0,0 +1,37 @@
+set term png size 2000,1250
+set output "perf1.png"
+
+set grid
+
+set datafile separator ";"
+
+set auto x
+
+set key right top
+
+set xlabel "Compiler"
+set ylabel "MiB/s" 
+
+set style data histogram
+set style fill solid border -1
+set boxwidth 0.5
+
+set yrange [0:100]
+
+set xtic rotate by -45 scale 0 
+
+set multiplot layout 3,3 rowsfirst title "Dotprod (size 100)" 
+
+set title "BASE"
+plot "data/BASE.dat" u 2:xtic(1) t "-O1" lc "blue", "" u 3:xtic(1) t "-O2" lc "orange", "" u 4:xtic(1) t "-O3" lc "red"
+
+set title "UNROLL4"
+plot "data/UNROLL4.dat" u 2:xtic(1) t "-O1" lc "blue", "" u 3:xtic(1) t "-O2" lc "orange", "" u 4:xtic(1) t "-O3" lc "red"
+
+set title "UNROLL8"
+plot "data/UNROLL8.dat" u 2:xtic(1) t "-O1" lc "blue", "" u 3:xtic(1) t "-O2" lc "orange", "" u 4:xtic(1) t "-O3" lc "red"
+
+set title "UNROLL16"
+plot "data/UNROLL16.dat" u 2:xtic(1) t "-O1" lc "blue", "" u 3:xtic(1) t "-O2" lc "orange", "" u 4:xtic(1) t "-O3" lc "red"
+
+unset multiplot
